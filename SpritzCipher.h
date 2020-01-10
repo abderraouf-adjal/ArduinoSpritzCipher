c7d19d4b2b7c4146eeea4e347d2a8a347aac87ee
abderraouf-adjal <abderraouf.adjal@gmail.com>
2020-01-10 21:25:58 +0100

    v1.0.6

--- SpritzCipher.h
+++ SpritzCipher.h
@@ -73,10 +73,10 @@ extern "C" {
 #define SPRITZ_N 256
 
 /* `Semantic Versioning` of this library */
-#define SPRITZ_LIBRARY_VERSION_STRING "1.0.5"
+#define SPRITZ_LIBRARY_VERSION_STRING "1.0.6"
 #define SPRITZ_LIBRARY_VERSION_MAJOR 1
 #define SPRITZ_LIBRARY_VERSION_MINOR 0
-#define SPRITZ_LIBRARY_VERSION_PATCH 5
+#define SPRITZ_LIBRARY_VERSION_PATCH 6
 
 
 /** spritz_ctx
