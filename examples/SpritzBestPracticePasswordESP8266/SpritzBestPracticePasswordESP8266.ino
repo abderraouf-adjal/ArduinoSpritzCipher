b2c6f7cd7b82895160a5c9c435777bf6e1201a57
abderraouf-adjal <abderraouf.adjal@gmail.com>
2020-01-10 21:21:58 +0100

    Better SpritzBestPracticePasswordESP8266 example

--- examples/SpritzBestPracticePasswordESP8266/SpritzBestPracticePasswordESP8266.ino
+++ examples/SpritzBestPracticePasswordESP8266/SpritzBestPracticePasswordESP8266.ino
@@ -22,12 +22,10 @@
 #include <SpritzCipher.h>
 #include <ESP8266WiFi.h>
 
-/* The RNG seed */
-volatile uint32_t hw_rng;
-uint8_t entropy_buf[256]; /* You can also use malloc() & free(). */
+
 
 /* Table of alphabetic (uppercase and lowercase) and numeric characters */
-const uint8_t alphanumeric_table[62] =
+const char alphanumeric_table[62] =
 { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -35,66 +33,54 @@ const uint8_t alphanumeric_table[62] =
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 };
 
+#define PASSWORD_LEN 28 /* ~160-bit of entropy password; ceil(160/log2(26*2+10)) */
+/* One spritz_ctx (One mem buffer) to save some memory.
+ * For both, hash function and the random numbers generator.
+ */
+spritz_ctx the_ctx;
 
 void setup() {
+  volatile uint32_t hw_rng; /* The RNG seed */
+  uint8_t buf[32];
+
   /* Initialize serial and wait for port to open */
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; /* Wait for serial port to connect. Needed for Leonardo only */
   }
-}
-
-
-void loop() {
-  uint8_t password_len = 28; /* ~160-bit of entropy password; ceil(160/log2(26*2+10)) */
-  uint8_t buf[32];
-  spritz_ctx the_ctx;
-  /* One spritz_ctx (One mem buffer) to save some memory.
-   * For both, hash function and the random numbers generator.
-   */
-
-  for (size_t i = 0; i < (sizeof(entropy_buf)/4); i += 4) {
-    delay(1);
-    hw_rng = *(volatile uint32_t *)0x3FF20E44;
-    entropy_buf[i]   = ((uint8_t *) &hw_rng)[0];
-    entropy_buf[i+2] = ((uint8_t *) &hw_rng)[1];
-    entropy_buf[i+3] = ((uint8_t *) &hw_rng)[2];
-    entropy_buf[i+4] = ((uint8_t *) &hw_rng)[3];
-  }
-  
 
   /* We did NOT use spritz_hash() cause it will have its own spritz_ctx,
    * And we like to save memory
    */
-  /* Make a 256-bit hash of the entropy in "buf" using one function */
+  /* To make a 256-bit hash of the entropy */
   spritz_hash_setup(&the_ctx); /* Initialize */
-  spritz_hash_update(&the_ctx, entropy_buf, (uint16_t)(sizeof(entropy_buf))); /* Add data */
-  spritz_hash_final(&the_ctx, buf, (uint8_t)(sizeof(buf))); /* Output the final hash */
+  
+  /* Insert 64*4=256 bytes of entropy in hash function */
+  for (uint16_t i = 0; i < 64; i++) {
+    delay(1);
+    hw_rng = *(volatile uint32_t *)0x3FF20E44;
+    spritz_hash_update(&the_ctx, (const uint8_t *)&hw_rng, (uint16_t)(sizeof(hw_rng))); /* Add 4 bytes of entropy */
+  }
 
-  spritz_state_memzero(&the_ctx); /* wipe "the_ctx" data by replacing it with zeros (0x00) */
-  spritz_memzero(entropy_buf, (uint16_t)(sizeof(entropy_buf))); /* wipe "entropy" data by replacing it with zeros (0x00) */
-  spritz_memzero((uint8_t *)hw_rng, (uint16_t)(sizeof(hw_rng)));
+  spritz_hash_final(&the_ctx, buf, (uint8_t)(sizeof(buf))); /* Output the final hash */
+  spritz_memzero((uint8_t *)&hw_rng, (uint16_t)(sizeof(hw_rng)));
 
   /* Initialize/Seed the RNG with the hash of entropy */
-  spritz_setup(&the_ctx, buf, (uint8_t)(sizeof(buf)));
-
+  spritz_setup(&the_ctx, buf, (uint8_t)(sizeof(buf))); /* wipe "the_ctx" data by replacing it with zeros (0x00) */
   spritz_memzero(buf, (uint16_t)(sizeof(buf))); /* wipe "buf" data by replacing it with zeros (0x00) */
 
+  Serial.println(F("\n\n[Make strong Alphanumeric passwords]\n\n"));
+}
 
-  Serial.println("\n[Make strong Alphanumeric passwords]\n");
 
-  while (1)
-  {
-    /* Fill the buffer with random uniformly distributed alphanumeric characters */
-    for (uint8_t i = 0; i < password_len; i++) {
-      /* Like: buf[i] = charactersTable[random() % number_Of_elements_In_charactersTable] */
-      buf[i] = alphanumeric_table[spritz_random32_uniform(&the_ctx, (uint32_t)(sizeof(alphanumeric_table)))];
-    }
-
-    /* Print the password */
-    Serial.write(buf, password_len);
-    Serial.println();
-
-    delay(1000); /* Wait 1s */
+void loop() {
+  /* Print random uniformly distributed alphanumeric characters.
+   * Better than: rand_char = charactersTable[random() % number_Of_elements_In_charactersTable]
+   */
+  for (uint8_t i = 0; i < PASSWORD_LEN; i++) {
+    Serial.print((char)alphanumeric_table[spritz_random32_uniform(&the_ctx, (uint32_t)(sizeof(alphanumeric_table)))]);
   }
+  Serial.println();
+
+  delay(1000); /* Wait 1s */
 }
